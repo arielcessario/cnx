6a34929c52adff4691253e02c44f7688ea96f586
meserovirtual <meserovirtualarg@gmail.com>
2016-09-21 15:56:14 -0300

    Ariel

--- cnx.php
+++ cnx.php
@@ -25,12 +25,17 @@ function get($key)
 //    if (!isset($this->db)) {
     switch ($key) {
         case 'bayres':
-            return new MysqliDb ('127.0.0.1', 'root', 'concentrador', 'arielces_bayres');
+            return new MysqliDb ('192.185.4.175', 'arielces_bayres', 't)htl)lPH{83', 'arielces_bayres');
+            break;
+        case 'bayres-test':
+            return new MysqliDb ('192.185.4.175', 'arielces_test', '*DJ[c(n@)[ku', 'arielces_bayres_test');
             break;
         case 'mps':
             return new MysqliDb ('127.0.0.1', 'root', 'concentrador', 'arielces_bayres');
+            break;
         default:
             return new MysqliDb ('127.0.0.1', 'root', 'concentrador', 'arielces_bayres');
+            break;
     }
 //    }
 }
